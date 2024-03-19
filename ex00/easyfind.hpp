198b35e843ff66675027f9de83ed06119b3f2a85
Youssef Bouchra <ybouchra@e3r5p2.1337.ma>
2024-03-19 08:19:01 +0000

    Tue Mar 19 08:19:01 +00 2024

---
+++ ex00/easyfind.hpp
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   easyfind.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ybouchra <ybouchra@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/19 07:15:25 by ybouchra          #+#    #+#             */
+/*   Updated: 2024/03/19 07:17:41 by ybouchra         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include <iostream>
+#include <algorithm>
+
+template <typename T>
+T easyfind(T first, int second)
+{
+    
+}
