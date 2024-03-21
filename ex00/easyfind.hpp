2dffceb38a4fbc4b7f9eff9fe3a61bb1df424011
ussefbouchraa <ussefbouchraa@gmail.com>
2024-03-21 17:55:04 +0000

    Thu 21 Mar 2024 05:55:04 PM +00

--- ex00/easyfind.hpp
+++ ex00/easyfind.hpp
@@ -3,20 +3,31 @@
 /*                                                        :::      ::::::::   */
 /*   easyfind.hpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ybouchra <ybouchra@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ussef <ussef@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/19 07:15:25 by ybouchra          #+#    #+#             */
-/*   Updated: 2024/03/19 07:17:41 by ybouchra         ###   ########.fr       */
+/*   Updated: 2024/03/21 17:00:37 by ussef            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
 #include <iostream>
+#include <vector>
+#include <list>
 #include <algorithm>
 
 template <typename T>
-T easyfind(T first, int second)
+void easyfind(T container, int value)
 {
     
+    typename T::iterator it;
+    for( it = container.begin(); it != container.end(); it++){
+        if (*it == value) {
+            std::cout << "Item is Found ." << std::endl;
+            return;
+        }
+    }
+        throw(std::exception()); 
+
 }
---
+++ ex00/main.cpp
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ussef <ussef@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/21 13:10:29 by ussef             #+#    #+#             */
+/*   Updated: 2024/03/21 17:52:26 by ussef            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "easyfind.hpp"
+
+
+
+int main()
+{
+    try
+    {
+        std::vector <int> vec;
+        vec.push_back(10);
+        vec.push_back(20);
+        vec.push_back(30);
+        easyfind<std::vector<int> >(vec, 50);
+    }
+    catch(const std::exception& ex)
+    {
+        std::cerr<< "Item Not Found ." << '\n';
+    }
+
+    std::cout << "-------------------------\n";
+    try
+    {
+        std::list <int> lst;
+        lst.push_back(100);
+        lst.push_back(200);
+        lst.push_back(300);
+        easyfind<std::list<int> >(lst, 200);
+    }
+    catch(const std::exception& ex)
+    {
+        std::cerr<< "Item Not Found ." << '\n';
+    }
+    
+}
